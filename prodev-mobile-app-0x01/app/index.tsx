942fef89064502b2cb0cc85e7e8cb0eb7f44ba0a
sahe <jackitehm@gmail.com>
2025-07-18 16:43:42 +0400

    Trying the checker

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -16,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    // justifyContent: "center",
+    // alignItems: "center",
     backgroundColor: "#90caf9",
   },
   largeText: {
